54480afd83919e730280c3c2baf9650300ad3c84
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-06-05 20:43:41 -0700

    Kinda working

--- lib/pagrant/project.rb
+++ lib/pagrant/project.rb
@@ -1,25 +1,40 @@
+require 'erb'
+
 module Pagrant
   class Project
     def initialize(path)
       @path = path
     end
 
-    def walk_tree(path)
-      Dir.entries(path).each do |entry|
+    def walk_tree(source, dest)
+      Dir.entries(source).each do |entry|
         unless [".", ".."].include?(entry)
-          newpath = File.join(path, entry)
+          newsource = File.join(source, entry)
+          newdest = File.join(dest, entry)
 
-          if Dir.exists?(newpath)
-            walk_tree(newpath)
+          if Dir.exists?(newsource)
+            Dir.mkdir(newdest) unless Dir.exists?(newdest)
+            walk_tree(newsource, newdest)
           else
-            puts entry
+            newfile = newdest[0..-5]
+
+            puts newsource
+            puts newfile
+            puts
+
+            template = ERB.new(File.read(newsource))
+
+            File.open(newfile, 'w') do |f|
+              f.write(template.result(binding))
+            end
           end
         end
       end
     end
 
     def generate
-      walk_tree(TEMPLATE_DIR)
+      Dir.mkdir(@path) unless Dir.exists?(@path)
+      walk_tree(TEMPLATE_DIR, @path)
     end
   end
 end
